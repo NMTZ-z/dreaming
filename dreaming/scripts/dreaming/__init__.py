fa338360b0cfdf0ea6f781828bedf27fd008e675
NMTZ-z <zhongshengyuan@tju.edu.cn>
2026-05-06 15:32:20 +0800

    Add dreaming/scripts/dreaming/__init__.py

---
+++ dreaming/scripts/dreaming/__init__.py
@@ -0,0 +1,4 @@
+from .main import run_dreaming
+from .sleep import promote_fragments, detect_skill_candidates, archive_stale_entries, recall_check
+from .memory_writer import insert_fragments, get_token_estimate
+from .storage import init_db
